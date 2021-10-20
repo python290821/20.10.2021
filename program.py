7ef9ddc860ee01a08a60e678ec33324faf81cbda
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-20 19:50:47 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,1 +1,16 @@
-##
+def get_1():
+    return 1 # breakout of the function
+    print() # this command will never execute
+
+def get_baribua( x ):
+    print(x**2)
+    return x**3
+
+y = get_baribua(3)
+print(f'y = {y}')
+print(get_1())
+x = get_1()
+print(f'function result: {get_1()}')
+get_1()
+
+
