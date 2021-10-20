0d214d7bff6822a96ff0cdc0a65fbb6f0d9ff75f
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-20 20:43:45 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,16 +1,19 @@
 import math
 
-def getCircleArea(radius):
-    #_area = 3.14 * radius ** 2
-    _area = math.pi * radius ** 2
+def getCircleArea(r):
+    _area = math.pi * r ** 2
     return _area
+
+def getCircleHekef(r):
+    _hekef = 2 * math.pi * r
+    return _hekef
 
 print(f'pi = {math.pi}')
 radius = float(input('please enter radius [f]: '))
-area = getCircleArea(radius)
-
-def getCircleHekef(radius):
-    pass # 2 * pi (3.14) * radius
+circle_area = getCircleArea(radius)
+circle_hekef = getCircleHekef(radius)
+print(f'circle radius {radius}, area: {circle_area}')
+print(f'circle radius {radius}, hekef: {circle_hekef}')
 
 def calcMalbenArea(width, height):
     pass # width * height
@@ -19,3 +22,22 @@ def calcMalbenHekef(width, height):
     pass # 2 * width + 2 * height
 # input from user float radius -- print the hekef and area
 # input from user width and height -- print the area and hekef
+
+def even_or_odd(x):
+    pass # return 'even' if even or 'odd'
+
+def get_factorial(x):
+    pass # return factorial atzret . x = 5 => 1 * 2 * 3 * 4 * 5
+
+def check_equal():
+    # get two parameters
+    # if they are equal return True otherwise false
+    # if only 1 number was sent then compare it to zero
+    pass #
+
+def get_full_name():
+    # get two parameters: fname , lname
+    # if i.e. avi levi was sent return 'avi levi'
+    # if i.e. only fname was send return 'avi doh'
+    # if t.e. none was send return 'john doh'
+    pass
