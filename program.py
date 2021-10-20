99be85c0d47491498214304582f0d361f24a9709
python290821 <90146835+python290821@users.noreply.github.com>
2021-10-20 19:57:25 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,1 +1,14 @@
-##
+
+def getCircleArea(radius):
+    pass # pi (3.14) * radius baribua
+
+def getCircleHekef(radius):
+    pass # 2 * pi (3.14) * radius
+
+def calcMalbenArea(width, height):
+    pass # width * height
+
+def calcMalbenHekef(width, height):
+    pass # 2 * width + 2 * height
+# input from user radius -- print the hekef and area
+# input from user width and height -- print the area and hekef
